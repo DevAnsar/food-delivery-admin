fe2b5cb9c05aa200c388041f6387977a5ddfbddc
DevAnsar <ansarmirzayi@gmail.com>
2022-01-21 19:03:20 +0330

    first commit

---
+++ src/components/_dashboard/app/index.js
@@ -0,0 +1,4 @@
+export { default as AppBugReports } from './AppBugReports';
+export { default as AppItemOrders } from './AppItemOrders';
+export { default as AppNewUsers } from './AppNewUsers';
+export { default as AppWeeklySales } from './AppWeeklySales';
---
+++ src/pages/DashboardApp.js
@@ -0,0 +1,38 @@
+// material
+import { Box, Grid, Container, Typography } from '@mui/material';
+// components
+import Page from '../components/Page';
+import {
+  AppNewUsers,
+  AppBugReports,
+  AppItemOrders,
+  AppWeeklySales
+} from '../components/_dashboard/app';
+
+// ----------------------------------------------------------------------
+
+export default function DashboardApp() {
+  return (
+    <Page title="Dashboard | Detal-Admin">
+      <Container maxWidth="xl">
+        <Box sx={{ pb: 5 }}>
+          <Typography variant="h4">Hi, Welcome back</Typography>
+        </Box>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} md={3}>
+            <AppWeeklySales />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <AppNewUsers />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <AppItemOrders />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <AppBugReports />
+          </Grid>
+        </Grid>
+      </Container>
+    </Page>
+  );
+}
